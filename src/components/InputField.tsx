8792e25785a48747f4c8fd0c4b56e6354f677e1b
Nat <fthbby@gmail.com>
2022-05-23 17:27:42 -0700

    todos array is saved into local storage

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -4,17 +4,18 @@ import './styles.css'
 interface Props{
     todo: string;
     setTodo: React.Dispatch<React.SetStateAction<string>>
+    handleAdd: (e: React.FormEvent)=>void;
 }
 
-const InputField: React.FC<Props> = ({todo, setTodo})=> {
+const InputField: React.FC<Props> = ({todo, setTodo, handleAdd})=> {
   return (
-    <form className='input'>
+    <form className='input' onSubmit={handleAdd}>
         <input type='input'
             value={todo}
             onChange={
                 (e) => setTodo(e.target.value)}
             placeholder='enter a task' className='input__box'/>
-            
+
         <button className='input_submit' type='submit'> Go</button>
     </form>
   )
